2dc187c35ff3532a1daabe323da35bd28f9fd8b6
toxa16 <toxa16@gmail.com>
2020-03-26 10:17:22 +0200

    init evaluation endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const evalStub = require('./eval-stub');
+
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,6 +25,12 @@ app.post('/forecast', (req, res) => {
 app.post('/email', (req, res) => {
   console.log(req.body);  // logging
   res.status(201).send();
+});
+
+app.post('/evaluation', (req, res) => {
+  console.log('incoming request...')
+  res.json(evalStub);
+  //res.status(400).end();
 });
 
 module.exports = app;
